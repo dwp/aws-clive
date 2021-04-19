450bfc9ffb38aef7d7366fb63bc1422c5d8f3e20
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-04-19 15:12:06 +0100

    simple echo for now to avoid shellcheck issues
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- steps/run-clive.sh
+++ steps/run-clive.sh
@@ -9,5 +9,6 @@ SERDE="${serde}"
 INPUT_DATE=$(date '+%Y-%m-%d')
 RAW_DIR="${data_path}"
 
+echo "$TARGET_DB" "$SERDE" "$INPUT_DATE" "$RAW_DIR"
 #currently commented out as not fully ready to run
 #/$CLIVE_LOCATION/scripts/build_clive.sh "$TARGET_DB" "$SERDE" "$INPUT_DATE" "$RAW_DIR"
