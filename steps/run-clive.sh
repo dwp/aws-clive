d607726318fe8da2ca8eeb4e1639220c8e6438b9
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2021-04-27 09:45:22 +0100

    added logs to step files and permissions to e2e prefix (#5)
    * added logs to step files and permissions to e2e prefix
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * put ci to normal and new line
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    Co-authored-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- steps/run-clive.sh
+++ steps/run-clive.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
-CLIVE_LOCATION=/opt/emr/dataworks-clive
+(
+    # Import the logging functions
+    source /opt/emr/logging.sh
 
-chmod u+x $CLIVE_LOCATION/scripts/build_clive.sh
+    function log_wrapper_message() {
+        log_aws_clive_message "$${1}" "run-clive.sh" "Running as: ,$USER"
+    }
 
-S3_PREFIX_FILE=/opt/emr/s3_prefix.txt
-S3_PREFIX=$(cat $S3_PREFIX_FILE)
+    CLIVE_LOCATION=/opt/emr/dataworks-clive
 
-PUBLISHED_BUCKET="${published_bucket}"
-TARGET_DB=${target_db}
-SERDE="${serde}"
-RAW_DIR="$PUBLISHED_BUCKET"/"$S3_PREFIX"
+    chmod u+x $CLIVE_LOCATION/scripts/build_clive.sh
 
-/$CLIVE_LOCATION/scripts/build_clive.sh "$TARGET_DB" "$SERDE" "$RAW_DIR"
+    S3_PREFIX_FILE=/opt/emr/s3_prefix.txt
+    S3_PREFIX=$(cat $S3_PREFIX_FILE)
+
+    PUBLISHED_BUCKET="${published_bucket}"
+    TARGET_DB=${target_db}
+    SERDE="${serde}"
+    RAW_DIR="$PUBLISHED_BUCKET"/"$S3_PREFIX"
+
+    log_wrapper_message "Set the following. published_bucket: $PUBLISHED_BUCKET, target_db: $TARGET_DB, serde: $SERDE, raw_dir: $RAW_DIR"
+
+    log_wrapper_message "Starting Clive job"
+
+    /$CLIVE_LOCATION/scripts/build_clive.sh "$TARGET_DB" "$SERDE" "$RAW_DIR"
+
+    log_wrapper_message "Finished Clive job"
+
+) >> /var/log/aws-clive/run-clive.log 2>&1
