17d18703327995c032a0d843ba667a5ba61cf4f6
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2021-04-29 08:32:00 +0100

    Dw 6047 (#7)
    * pushing dynamo script
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * parse args from emr launcher
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * change dir of script
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * change the values used in dynamo script
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * change key for s3 prefix
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * remove output location as not necessary for clive
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * -
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * follow pdm example
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * remove pdm refs
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    Co-authored-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- steps/create-clive-databases.sh
+++ steps/create-clive-databases.sh
@@ -10,12 +10,16 @@ set -Eeuo pipefail
         log_aws_clive_message "$${1}" "create-clive-databases.sh" "Running as: ,$USER"
     }
 
+
     CORRELATION_ID="$2"
     S3_PREFIX="$4"
-
-    # Set the ddb values as this is the initial step
+    SNAPSHOT_TYPE="$6"
+    EXPORT_DATE="$8"
+    
     echo "$CORRELATION_ID" >>     /opt/emr/correlation_id.txt
     echo "$S3_PREFIX" >>          /opt/emr/s3_prefix.txt
+    echo "$SNAPSHOT_TYPE" >>      /opt/emr/snapshot_type.txt
+    echo "$EXPORT_DATE" >>        /opt/emr/export_date.txt
 
     log_wrapper_message "Creating Clive Databases"
 
