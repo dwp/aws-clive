2845d5974f364bc7a4e9a2dcd5ccece318c9da5a
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2021-05-04 09:19:02 +0100

    adding cloudwatch alerts on clive EMR termination status (#9)
    * adding cloudwatch alerts on clive EMR termination status
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * get rid of slack alerts file as cloudwatch file is more correct
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    Co-authored-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- cloudwatch_events.tf
+++ cloudwatch_events.tf
@@ -1,6 +1,6 @@
-resource "aws_cloudwatch_event_rule" "aws_clive_failed" {
-  name          = "aws_clive_failed"
-  description   = "Sends failed message to slack when aws_clive cluster terminates with errors"
+resource "aws_cloudwatch_event_rule" "clive_failed" {
+  name          = "clive_failed"
+  description   = "Sends failed message to slack when clive cluster terminates with errors"
   event_pattern = <<EOF
 {
   "source": [
@@ -14,16 +14,16 @@ resource "aws_cloudwatch_event_rule" "aws_clive_failed" {
       "TERMINATED_WITH_ERRORS"
     ],
     "name": [
-      "aws_clive"
+      "aws-clive"
     ]
   }
 }
 EOF
 }
 
-resource "aws_cloudwatch_event_rule" "aws_clive_terminated" {
-  name          = "aws_clive_terminated"
-  description   = "Sends failed message to slack when aws_clive cluster terminates by user request"
+resource "aws_cloudwatch_event_rule" "clive_terminated" {
+  name          = "clive_terminated"
+  description   = "Sends terminated message to slack when clive cluster terminates by user request"
   event_pattern = <<EOF
 {
   "source": [
@@ -37,7 +37,7 @@ resource "aws_cloudwatch_event_rule" "aws_clive_terminated" {
       "TERMINATED"
     ],
     "name": [
-      "aws_clive"
+      "aws-clive"
     ],
     "stateChangeReason": [
       "{\"code\":\"USER_REQUEST\",\"message\":\"User request\"}"
@@ -47,8 +47,8 @@ resource "aws_cloudwatch_event_rule" "aws_clive_terminated" {
 EOF
 }
 
-resource "aws_cloudwatch_event_rule" "aws_clive_success" {
-  name          = "aws_clive_success"
+resource "aws_cloudwatch_event_rule" "clive_success" {
+  name          = "clive_success"
   description   = "checks that all steps complete"
   event_pattern = <<EOF
 {
@@ -63,7 +63,7 @@ resource "aws_cloudwatch_event_rule" "aws_clive_success" {
       "TERMINATED"
     ],
     "name": [
-      "aws_clive"
+      "aws-clive"
     ],
     "stateChangeReason": [
       "{\"code\":\"ALL_STEPS_COMPLETED\",\"message\":\"Steps completed\"}"
@@ -73,9 +73,9 @@ resource "aws_cloudwatch_event_rule" "aws_clive_success" {
 EOF
 }
 
-resource "aws_cloudwatch_event_rule" "aws_clive_running" {
-  name          = "aws_clive_running"
-  description   = "checks that aws_clive is running"
+resource "aws_cloudwatch_event_rule" "clive_running" {
+  name          = "clive_running"
+  description   = "checks that clive has started running"
   event_pattern = <<EOF
 {
   "source": [
@@ -89,16 +89,16 @@ resource "aws_cloudwatch_event_rule" "aws_clive_running" {
       "RUNNING"
     ],
     "name": [
-      "aws_clive"
+      "aws-clive"
     ]
   }
 }
 EOF
 }
 
-resource "aws_cloudwatch_metric_alarm" "aws_clive_failed" {
-  count                     = local.aws_clive_alerts[local.environment] == true ? 1 : 0
-  alarm_name                = "aws_clive_failed"
+resource "aws_cloudwatch_metric_alarm" "clive_failed" {
+  count                     = local.clive_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "clive_failed"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "TriggeredRules"
@@ -106,25 +106,25 @@ resource "aws_cloudwatch_metric_alarm" "aws_clive_failed" {
   period                    = "60"
   statistic                 = "Sum"
   threshold                 = "1"
-  alarm_description         = "This metric monitors cluster failed with errors"
+  alarm_description         = "This metric monitors cluster termination with errors"
   insufficient_data_actions = []
   alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
   dimensions = {
-    RuleName = aws_cloudwatch_event_rule.aws_clive_failed.name
+    RuleName = aws_cloudwatch_event_rule.clive_failed.name
   }
   tags = merge(
     local.common_tags,
     {
-      Name              = "aws_clive_failed",
+      Name              = "clive_failed",
       notification_type = "Error",
       severity          = "Critical"
     },
   )
 }
 
-resource "aws_cloudwatch_metric_alarm" "aws_clive_terminated" {
-  count                     = local.aws_clive_alerts[local.environment] == true ? 1 : 0
-  alarm_name                = "aws_clive_terminated"
+resource "aws_cloudwatch_metric_alarm" "clive_terminated" {
+  count                     = local.clive_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "clive_terminated"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "TriggeredRules"
@@ -136,21 +136,21 @@ resource "aws_cloudwatch_metric_alarm" "aws_clive_terminated" {
   insufficient_data_actions = []
   alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
   dimensions = {
-    RuleName = aws_cloudwatch_event_rule.aws_clive_terminated.name
+    RuleName = aws_cloudwatch_event_rule.clive_terminated.name
   }
   tags = merge(
     local.common_tags,
     {
-      Name              = "aws_clive_terminated",
+      Name              = "clive_terminated",
       notification_type = "Information",
       severity          = "High"
     },
   )
 }
 
-resource "aws_cloudwatch_metric_alarm" "aws_clive_success" {
-  count                     = local.aws_clive_alerts[local.environment] == true ? 1 : 0
-  alarm_name                = "aws_clive_success"
+resource "aws_cloudwatch_metric_alarm" "clive_success" {
+  count                     = local.clive_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "clive_success"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "TriggeredRules"
@@ -158,25 +158,25 @@ resource "aws_cloudwatch_metric_alarm" "aws_clive_success" {
   period                    = "60"
   statistic                 = "Sum"
   threshold                 = "1"
-  alarm_description         = "Monitoring aws_clive completion"
+  alarm_description         = "Monitoring clive completion"
   insufficient_data_actions = []
   alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
   dimensions = {
-    RuleName = aws_cloudwatch_event_rule.aws_clive_success.name
+    RuleName = aws_cloudwatch_event_rule.clive_success.name
   }
   tags = merge(
     local.common_tags,
     {
-      Name              = "aws_clive_success",
+      Name              = "clive_success",
       notification_type = "Information",
       severity          = "Critical"
     },
   )
 }
 
-resource "aws_cloudwatch_metric_alarm" "aws_clive_running" {
-  count                     = local.aws_clive_alerts[local.environment] == true ? 1 : 0
-  alarm_name                = "aws_clive_running"
+resource "aws_cloudwatch_metric_alarm" "clive_running" {
+  count                     = local.clive_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "clive_running"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "TriggeredRules"
@@ -184,16 +184,16 @@ resource "aws_cloudwatch_metric_alarm" "aws_clive_running" {
   period                    = "60"
   statistic                 = "Sum"
   threshold                 = "1"
-  alarm_description         = "Monitoring aws_clive running"
+  alarm_description         = "Monitoring clive completion"
   insufficient_data_actions = []
   alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
   dimensions = {
-    RuleName = aws_cloudwatch_event_rule.aws_clive_running.name
+    RuleName = aws_cloudwatch_event_rule.clive_running.name
   }
   tags = merge(
     local.common_tags,
     {
-      Name              = "aws_clive_running",
+      Name              = "clive_running",
       notification_type = "Information",
       severity          = "Critical"
     },
