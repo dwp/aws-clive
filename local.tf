83c94e9e979e4b695aaefd1d3e70a1bf3955cd74
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-05-12 19:15:51 +0100

    bump clive release and point to new parallel script
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- local.tf
+++ local.tf
@@ -61,11 +61,11 @@ locals {
   }
 
   dataworks_clive_version = {
-    development = "0.0.9"
-    qa          = "0.0.9"
-    integration = "0.0.9"
-    preprod     = "0.0.9"
-    production  = "0.0.9"
+    development = "0.0.11"
+    qa          = "0.0.11"
+    integration = "0.0.11"
+    preprod     = "0.0.11"
+    production  = "0.0.11"
   }
 
   clive_alerts = {
