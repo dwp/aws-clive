4bff22f8406307a79ab2e25b976dec0d639ca9c5
steveburton4 <Steven.burton@hotmail.co.uk>
2021-06-01 11:39:10 +0100

    More the service discovery to repo (#32)

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,10 @@ output "aws_clive_common_sg" {
 output "aws_clive_emr_launcher_lambda" {
   value = aws_lambda_function.aws_clive_emr_launcher
 }
+
+output "private_dns" {
+  value = {
+    clive_service_discovery_dns = aws_service_discovery_private_dns_namespace.clive_services
+    clive_service_discovery     = aws_service_discovery_service.clive_services
+  }
+}
