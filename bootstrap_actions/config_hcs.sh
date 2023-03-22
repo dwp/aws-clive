27ea1770831bef06b64e863e9d75d96c032583ec
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-03-22 12:24:40 +0000

    chore: Amending Tenable script

--- bootstrap_actions/config_hcs.sh
+++ bootstrap_actions/config_hcs.sh
@@ -1,48 +1,63 @@
 #!/bin/bash
 
-ec2-get-tag () {
-    if [ -z $1 ]; then
-        scriptName=`basename "$0"`
-        echo  >&2 "Usage: $scriptName <tag_name>"
-        exit 1
-    fi
+(
+    # Import the logging functions
+    source /opt/emr/logging.sh
 
-    # check that aws and ec2-metadata commands are installed
-    command -v aws >/dev/null 2>&1 || { echo >&2 'aws command not installed.'; exit 2; }
-    command -v ec2-metadata >/dev/null 2>&1 || { echo >&2 'ec2-metadata command not installed.'; exit 3; }
-
-    # set filter parameters
-    instanceId=$(ec2-metadata -i | cut -d ' ' -f2)
-    filterParams=( --filters "Name=key,Values=$1" "Name=resource-type,Values=instance" "Name=resource-id,Values=$instanceId" )
-
-    # get region
-    region=$(ec2-metadata --availability-zone | cut -d ' ' -f2)
-    region=${region%?}
-
-    # retrieve tags
-    tagValues=$(aws ec2 describe-tags --output text --region "$region" "${filterParams[@]}")
-    if [ $? -ne 0 ]; then
-        echo >&2 "Error retrieving tag value."
-        exit 4
-    fi
-
-    # extract required tag value
-    tagValue=$(echo "$tagValues" | cut -f5)
-    echo "$tagValue"
+    function log_wrapper_message() {
+        log_aws_clive_message "$${1}" "config_hcs.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
     }
 
-export TECHNICALSERVICE=$(ec2-get-tag Application)
-export ENVIRONMENT=$(ec2-get-tag Environment)
+    log_wrapper_message "Getting tags..."
+    ec2-get-tag () {
+        if [ -z $1 ]; then
+            scriptName=`basename "$0"`
+            echo  >&2 "Usage: $scriptName <tag_name>"
+            exit 1
+        fi
 
-echo $TECHNICALSERVICE
-echo $ENVIRONMENT
+        # check that aws and ec2-metadata commands are installed
+        command -v $(which aws) >/dev/null 2>&1 || { echo >&2 'aws command not installed.'; exit 2; }
+        command -v ec2-metadata >/dev/null 2>&1 || { echo >&2 'ec2-metadata command not installed.'; exit 3; }
 
-if [[ "$ENVIRONMENT" == "Dev" || "$ENVIRONMENT" == "DT_Tooling" ]]; then
-    export PROXY="egress.nonprod.dwpcloud.uk"
-  else
-    export PROXY="egress.service.dwpcloud.uk"
-fi
+        # set filter parameters
+        instanceId=$(ec2-metadata -i | cut -d ' ' -f2)
+        filterParams=( --filters "Name=key,Values=$1" "Name=resource-type,Values=instance" "Name=resource-id,Values=$instanceId" )
 
-/opt/nessus_agent/sbin/nessuscli agent link --key=$TENABLE_LINKING_KEY \ 
-  --cloud --groups=$TECHNICALSERVICE_$ENVIRONMENT,TVAT \ 
-  --proxy-host=$PROXY --proxy-port=3128
+        # get region
+        region=$(ec2-metadata --availability-zone | cut -d ' ' -f2)
+        region=${region%?}
+
+        # retrieve tags
+        tagValues=$($(which aws) ec2 describe-tags --output text --region "$region" "${filterParams[@]}")
+        if [ $? -ne 0 ]; then
+            echo >&2 "Error retrieving tag value."
+            exit 4
+        fi
+
+        # extract required tag value
+        tagValue=$(echo "$tagValues" | cut -f5)
+        echo "$tagValue"
+        }
+
+    export TECHNICALSERVICE=$(ec2-get-tag Application)
+    export ENVIRONMENT=$(ec2-get-tag Environment)
+
+    echo $TECHNICALSERVICE
+    echo $ENVIRONMENT
+
+    log_wrapper_message "Exporting proxy"
+
+    if [[ "$ENVIRONMENT" == "Dev" || "$ENVIRONMENT" == "DT_Tooling" ]]; then
+        export PROXY="egress.nonprod.dwpcloud.uk"
+      else
+        export PROXY="egress.service.dwpcloud.uk"
+    fi
+
+    log_wrapper_message "Configuring tenable agent"
+
+    /opt/nessus_agent/sbin/nessuscli agent link --key=$TENABLE_LINKING_KEY \ 
+      --cloud --groups=$TECHNICALSERVICE_$ENVIRONMENT,TVAT \ 
+      --proxy-host=$PROXY --proxy-port=3128
+
+)   >> /var/log/aws-clive/config_hcs.log 2>&1
