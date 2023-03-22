dfc2ecbfd17ea1af326092f3d8568057a2df791c
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-03-22 13:25:37 +0000

    chore: Updated with policy for getting tags

--- bootstrap_actions/config_hcs.sh
+++ bootstrap_actions/config_hcs.sh
@@ -17,19 +17,19 @@
         fi
 
         # check that aws and ec2-metadata commands are installed
-        command -v sudo $(which aws) >/dev/null 2>&1 || { echo >&2 'aws command not installed.'; exit 2; }
-        command -v sudo ec2-metadata >/dev/null 2>&1 || { echo >&2 'ec2-metadata command not installed.'; exit 3; }
+        command -v $(which aws) >/dev/null 2>&1 || { echo >&2 'aws command not installed.'; exit 2; }
+        command -v ec2-metadata >/dev/null 2>&1 || { echo >&2 'ec2-metadata command not installed.'; exit 3; }
 
         # set filter parameters
-        instanceId=$(sudo ec2-metadata -i | cut -d ' ' -f2)
+        instanceId=$(ec2-metadata -i | cut -d ' ' -f2)
         filterParams=( --filters "Name=key,Values=$1" "Name=resource-type,Values=instance" "Name=resource-id,Values=$instanceId" )
 
         # get region
-        region=$(sudo ec2-metadata --availability-zone | cut -d ' ' -f2)
+        region=$(ec2-metadata --availability-zone | cut -d ' ' -f2)
         region=${region%?}
 
         # retrieve tags
-        tagValues=$(sudo $(which aws) ec2 describe-tags --output text --region "$region" "${filterParams[@]}")
+        tagValues=$(ec2 describe-tags --output text --region "$region" "${filterParams[@]}")
         if [ $? -ne 0 ]; then
             echo >&2 "Error retrieving tag value."
             exit 4
