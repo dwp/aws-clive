7adb39b5abc5d1ca7c10820092bd97fcaf6008b3
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-03-23 10:20:57 +0000

    chore: testing

--- cluster_config.tf
+++ cluster_config.tf
@@ -55,8 +55,8 @@ resource "aws_s3_object" "steps" {
       action_on_failure   = local.step_fail_action[local.environment]
       s3_published_bucket = data.terraform_remote_state.common.outputs.published_bucket.id
       environment         = local.hcs_environment[local.environment]
-      proxy_http_host    = data.terraform_remote_state.internal_compute.outputs.internet_proxy.host
-      proxy_http_port    = data.terraform_remote_state.internal_compute.outputs.internet_proxy.port
+      proxy_http_host     = data.terraform_remote_state.internal_compute.outputs.internet_proxy.host
+      proxy_http_port     = data.terraform_remote_state.internal_compute.outputs.internet_proxy.port
     }
   )
 }
