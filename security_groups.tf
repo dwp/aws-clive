443081d1159cb309dc85145709b96cba0dca68de
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-08-10 12:44:02 +0100

    chore: Tanium sg and vpce config

--- security_groups.tf
+++ security_groups.tf
@@ -112,43 +112,43 @@ resource "aws_security_group_rule" "egress_aws_clive_to_dks" {
 
 
 resource "aws_security_group_rule" "clive_host_outbound_tanium_1" {
-  description       = "Clive host outbound port 1 to Tanium"
-  type              = "egress"
-  from_port         = var.tanium_port_1
-  to_port           = var.tanium_port_1
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.aws_clive_common.id
+  description              = "Clive host outbound port 1 to Tanium"
+  type                     = "egress"
+  from_port                = var.tanium_port_1
+  to_port                  = var.tanium_port_1
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.aws_clive_common.id
+  source_security_group_id = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
 }
 
 resource "aws_security_group_rule" "clive_host_outbound_tanium_2" {
-  description       = "Clive host outbound port 2 to Tanium"
-  type              = "egress"
-  from_port         = var.tanium_port_2
-  to_port           = var.tanium_port_2
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.aws_clive_common.id
+  description              = "Clive host outbound port 2 to Tanium"
+  type                     = "egress"
+  from_port                = var.tanium_port_2
+  to_port                  = var.tanium_port_2
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.aws_clive_common.id
+  source_security_group_id = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
 }
 
 resource "aws_security_group_rule" "clive_host_inbound_tanium_1" {
-  description       = "Clive host inbound port 1 from Tanium"
-  type              = "ingress"
-  from_port         = var.tanium_port_1
-  to_port           = var.tanium_port_1
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.aws_clive_common.id
+  description              = "Clive host inbound port 1 from Tanium"
+  type                     = "ingress"
+  from_port                = var.tanium_port_1
+  to_port                  = var.tanium_port_1
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
+  source_security_group_id = aws_security_group.aws_clive_common.id
 }
 
 resource "aws_security_group_rule" "clive_host_inbound_tanium_2" {
-  description       = "Clive host inbound port 2 from Tanium"
-  type              = "ingress"
-  from_port         = var.tanium_port_2
-  to_port           = var.tanium_port_2
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.aws_clive_common.id
+  description              = "Clive host inbound port 2 from Tanium"
+  type                     = "ingress"
+  from_port                = var.tanium_port_2
+  to_port                  = var.tanium_port_2
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
+  source_security_group_id = aws_security_group.aws_clive_common.id
 }
 
 resource "aws_security_group_rule" "ingress_to_dks" {
